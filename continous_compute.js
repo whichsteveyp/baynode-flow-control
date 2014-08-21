0df952bfaf055cd6b374586b1974930f4bfeb004
Stephen Rivas Jr <srivas@lyveminds.com>
2014-08-20 17:41:43 -0700

    updating example for setImmediate in poorly named file

--- continous_compute.js
+++ continous_compute.js
@@ -1,13 +1,19 @@
 
 var http = require('http');
+var fs   = require('fs');
+var path = require('path');
 
 // lets briefly touch on this
-// process.maxTickDepth = 5;
+process.maxTickDepth = 5;
 
 console.log(process.maxTickDepth);
 
+var lots_of_files = ['continous_compute.js'];
+
 function compute() {
-    console.log('doing some computes');
+    // do some stuff
+    var results = fs.readFileSync(path.join(__dirname, lots_of_files[0]));
+    console.log('computed a chunk of big stuff, sync-like, results: ' + results.length);
 
     // recursion blocks the process from responding to incoming requests
     // also, stack overflow
@@ -17,12 +23,15 @@ function compute() {
     // setTimeout(compute, 1000);
 
     // performs complicated calculations continuously
+    // but it's going to break, eventually (see maxTickDepth, setImmediate)
     // process.nextTick(compute);
 
-    // another thing, we'll maybe talk about this later
+    // this thing...we'll maybe talk about this later
     // setImmediate(compute);
 }
 
+// test with:
+// ab -n 10000 -c 10 http://127.0.0.1:5000/
 http.createServer(function(req, res) {
     console.log('------------------------------------ doing some response');
     res.writeHead(200, {'Content-Type': 'text/plain'});
